a81d6ea92526a9a9abf63d1eec3e7cb70e2696cf
Samuel <iesbsamuel@gmail.com>
2026-03-27 10:38:27 -0300

    adicionando aula 02

---
+++ aulas/aula02/agenda.h
@@ -0,0 +1,28 @@
+#ifndef AGENDA_H
+#define AGENDA_H
+
+#define LIMITE 100
+
+typedef struct contato_t {
+    char nome[101];
+    char telefone[61];
+} Contato;
+
+typedef struct agenda_t {
+    Contato contatos[LIMITE];
+    int tamanho;
+    int quantidade;
+    
+} Agenda;
+
+
+Agenda criar_agenda(int tamanho);
+void adicionar_contato(Agenda *agenda, Contato contato);
+void remover_contato(Agenda *agenda, Contato contato);
+void bloquear_contato(Agenda *agenda, Contato contato);
+void desbloquear_contato(Agenda *agenda, Contato contato);
+void editar_contato(Agenda *agenda, Contato contato);
+void listar_contato(Agenda *agenda);
+
+
+#endif 
---
+++ aulas/aula02/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "agenda.h"
+
+int main() {
+    Agenda agenda = criar_agenda(10);
+
+    Contato contato;
+    strcpy(contato.nome, "Jose");
+    strcpy(contato.telefone, "1111-1111");
+
+    adicionar_contato(&agenda, contato);
+
+    listar_contato(&agenda);
+
+    Contato outro;
+    strcpy(outro.nome, "Maria");
+    strcpy(outro.telefone, "2222-2222");
+
+    adicionar_contato(&agenda, outro);
+
+    listar_contato(&agenda);
+}
+
