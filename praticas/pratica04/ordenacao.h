8a5795c377f3d94184f130329fa54680a3c47285
moksha369 <iesbsamuel@gmail.com>
2026-04-23 04:56:25 -0300

    Feat: concluindo a pratica04

---
+++ praticas/pratica04/ordenacao.h
@@ -0,0 +1,8 @@
+#ifndef ORDENACAO_H
+#define ORDENACAO_H
+
+void bubble_sort(int vetor[], int tamanho);
+void selection_sort(int vetor[], int tamanho);
+void quick_sort(int vetor[], int inicio, int fim);
+ 
+#endif 
